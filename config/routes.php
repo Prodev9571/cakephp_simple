6b6c0fefdc28dc9b4ec521e732339cf5daad315e
Pro <prodev9433@outlook.com>
2020-12-29 22:22:38 +0900

    Update Route

--- config/routes.php
+++ config/routes.php
@@ -62,8 +62,9 @@ Router::scope('/', function (RouteBuilder $routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
-    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
-
+    $routes->connect('/', ['controller' => 'Contacts', 'action' => 'index', 'home']);
+    $routes->connect('/index', ['controller' => 'Contacts', 'action' => 'index', 'home']);
+    $routes->connect('/index_ext', ['controller' => 'Contacts', 'action' => 'index_ext', 'home']);
     /*
      * ...and connect the rest of 'Pages' controller's URLs.
      */
